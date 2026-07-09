9feae426fdfd82ad92061896bb6da56bf44bf50c
ONestoDev <ernestoo.es@gmail.com>
2026-07-09 03:39:41 -0300

    conclusão aula 1

--- codigo.py
+++ codigo.py
@@ -10,7 +10,7 @@ import pandas
 # pyautogui.press -> apertar alguma tecla do teclado
 # pyautogui.hotkey -> apertar uma combinação de teclas do teclado (atalhos)
 
-pyautogui.PAUSE = 2.5  # tempo de espera entre cada comando
+pyautogui.PAUSE = 3  # tempo de espera entre cada comando
 link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
 email = "seu_email@mail.com"
 senha = "sua_senha"
@@ -18,29 +18,31 @@ senha = "sua_senha"
 # Passo a passo do seu programa
 # Passo 1: entrar no sistema da empresa
 # abrir o navegador
-#pyautogui.press("win")
-#pyautogui.write("chrome")
-#pyautogui.press("enter")
-#pyautogui.write(link)
-#pyautogui.press("enter")
-# pausa para o navegador abrir a página
-#time.sleep(3)
-
-# para mac seria:
-pyautogui.hotkey("command", "space")
-pyautogui.write("safari") #ou safari
+pyautogui.press("win")
+pyautogui.write("chrome")
+pyautogui.press("enter")
+pyautogui.click(x=1388, y=686)
 pyautogui.press("enter")
 pyautogui.write(link)
 pyautogui.press("enter")
-time.sleep(5)
+# pausa para o navegador abrir a página
+time.sleep(4)
+
+#para mac seria:
+#pyautogui.hotkey("command", "space")
+#pyautogui.write("safari") #ou safari
+#pyautogui.press("enter")chrome
+#pyautogui.write(link)
+#pyautogui.press("enter")
+#time.sleep(5)
 
 # Passo 2: Fazer login
 #clicar no campo de email
-pyautogui.click(x=685, y=451)
+pyautogui.click(x=1183, y=411)
 pyautogui.write(email)
 pyautogui.press("tab")  # passar para o próximo campo
 pyautogui.write(senha)
-pyautogui.click("tab")  # passar para o botão de login
+pyautogui.press("tab")  # passar para o botão de login
 pyautogui.press("enter")
 time.sleep(4)
 # Passo 3: Abrir a base de dados
@@ -50,17 +52,17 @@ print(tabela)
 for linha in tabela.index:
     # Passo 4: Cadastrar 1 produto
     # codigo
-    pyautogui.click(x=653, y=294)  # clicar no campo de código
+    pyautogui.click(x=1104, y=293)  # clicar no campo de código
     codigo = tabela.loc[linha, "codigo"]  # pegar o código do produto da tabela
-    pyautogui.write(codigo)  # escrever o código do produto
+    pyautogui.write(str(codigo))  # escrever o código do produto
     pyautogui.press("tab")  # passar para o próximo campo
     # marca
     marca = tabela.loc[linha, "marca"]  # pegar a marca do produto da tabela
-    pyautogui.write(marca)  # escrever a marca do produto
+    pyautogui.write(str(marca))  # escrever a marca do produto
     pyautogui.press("tab")  # passar para o próximo campo
     # tipo
     tipo = tabela.loc[linha, "tipo"]  # pegar o tipo do produto da tabela
-    pyautogui.write(tipo)  # escrever o tipo do produto
+    pyautogui.write(str(tipo))  # escrever o tipo do produto
     pyautogui.press("tab")  # passar para o próximo campo
     # categoria
     categoria = tabela.loc[linha, "categoria"]  # pegar a categoria do produto da tabela
@@ -76,8 +78,8 @@ for linha in tabela.index:
     pyautogui.press("tab")  # passar para o próximo campo
     # Obs
     obs = tabela.loc[linha, "obs"]  # pegar as observações do produto da tabela
-    if obs != "nan":  # verificar se o campo de observações não está vazio
-        pyautogui.write(obs)  # escrever as observações do produto
+    if obs == "nan": # verificar se o campo de observações não está vazio
+        pyautogui.write(str(tabela.loc[linha, "obs"]))  # escrever as observações do produto
     pyautogui.press("tab")  # passar para o próximo campo
     pyautogui.press("enter")  # apertar o botão de cadastrar
 
